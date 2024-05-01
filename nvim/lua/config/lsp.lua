cf62f9cdfce75a0c513dbf791c9692d03bbdeca9
lightning <lightning004@outlook.com>
2024-05-02 02:06:58 +0800

    绑定快捷键lsp

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -16,7 +16,8 @@ require('mason-lspconfig').setup({
         'volar',
         'cssls',
         'emmet_ls',
-        'tsserver' },
+        'tsserver'
+    },
 })
 -- ...
 -- Set different settings for different languages' LSP
@@ -29,9 +30,9 @@ local lspconfig = require('lspconfig')
 -- Customized on_attach function
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', 'gk', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', 'gj', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
@@ -53,9 +54,9 @@ local on_attach = function(client, bufnr)
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, bufopts)
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', 'gp', vim.lsp.buf.references, bufopts)
     vim.keymap.set("n", "<space>f", function()
         vim.lsp.buf.format({ async = true })
     end, bufopts)
