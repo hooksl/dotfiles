ac83702852a075dee8b9bde7efaf6a66fcd2074c
lightning <lightning004@outlook.com>
2024-05-05 15:30:46 +0800

    关闭缩进函数横线

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,11 +2,18 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {},
-    -- config = function()
-    --     require("indent_blankline").setup {
-    --         -- for example, context is off by default, use this to turn it on
-    --         -- show_current_context = true,
-    --         -- show_current_context_start = true,
-    --     }
-    -- end,
+    config = function()
+        require("ibl").setup {
+            indent = {
+                char = "▏",
+            },
+            scope = {
+                -- 关掉函数上的横线
+                enabled = false
+            }
+            -- for example, context is off by default, use this to turn it on
+            -- show_current_context = true,
+            -- show_current_context_start = true,
+        }
+    end,
 }
