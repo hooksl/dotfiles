2cf7fb4e5520bfc95c79e8159518fe97226a479c
lightning <lightning004@outlook.com>
2024-05-06 03:31:34 +0800

    文件浏览器透明

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -18,8 +18,10 @@ return {
                     functions = {},
                     variables = {},
                     -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark",            -- style for sidebars, see below
-                    floats = "dark",              -- style for floating windows
+                    sidebars = "transparent",
+                    floats = "transparent",
+                    -- sidebars = "dark",            -- style for sidebars, see below
+                    -- floats = "dark",              -- style for floating windows
                 },
                 sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
                 day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
@@ -29,13 +31,13 @@ return {
 
                 --- You can override specific color groups to use other groups or a hex color
                 --- function will be called with a ColorScheme table
-                ---@param colors ColorScheme
+                -- @param colors ColorScheme
                 on_colors = function(colors) end,
 
                 --- You can override specific highlights to use other groups or a hex color
                 --- function will be called with a Highlights and ColorScheme table
-                --- @param highlights Highlights
-                --- @param colors ColorScheme
+                -- @param highlights Highlights
+                -- @param colors ColorScheme
                 on_highlights = function(highlights, colors) end,
 
             })
