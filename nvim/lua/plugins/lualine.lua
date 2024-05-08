03e7d77968b17fe253471360da13fd683a257097
lightning <lightning004@outlook.com>
2024-05-08 17:52:12 +0800

    状态栏显示lsp

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,6 +2,22 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
     config = function()
+        -- LSP clients attached to buffer
+        local clients_lsp = function()
+            local bufnr = vim.api.nvim_get_current_buf()
+
+            local clients = vim.lsp.buf_get_clients(bufnr)
+            if next(clients) == nil then
+                return ''
+                -- return 'no lsp'
+            end
+
+            local c = {}
+            for _, client in pairs(clients) do
+                table.insert(c, client.name)
+            end
+            return '\u{f085} ' .. table.concat(c, '|')
+        end
         require('lualine').setup {
             options = {
                 icons_enabled = true,
@@ -27,6 +43,7 @@ return {
                 lualine_c = { 'filename' },
                 lualine_x = {
                     -- { "diagnostics", sources = { "nvim_lsp" }, symbols = { error = " ", warn = " ", info = " ", hint = " " } },
+                    clients_lsp,
                     'encoding',
                     'fileformat',
                     'filetype'
