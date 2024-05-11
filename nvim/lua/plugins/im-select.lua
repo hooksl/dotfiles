f6a787d3e85c181b707372ee7e0be1cf9ce44b56
lightning <lightning004@outlook.com>
2024-05-11 15:15:42 +0800

    窗口事件触发输入法

--- nvim/lua/plugins/im-select.lua
+++ nvim/lua/plugins/im-select.lua
@@ -20,7 +20,7 @@ return {
             default_command         = 'fcitx5-remote',
 
             -- Restore the default input method state when the following events are triggered
-            set_default_events      = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
+            set_default_events      = { "WinEnter","VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
 
             -- Restore the previous used input method state when the following events
             -- are triggered, if you don't want to restore previous used im in Insert mode,
