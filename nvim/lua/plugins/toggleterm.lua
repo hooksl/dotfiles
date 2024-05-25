5ac1997e480714261fac02c888cae199a56e3d73
lightning <lightning004@outlook.com>
2024-05-26 00:44:16 +0800

    新增一个终端<c-f>

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -32,8 +32,8 @@ return {
         vim.api.nvim_set_keymap("t", "<c-g>", "<Cmd>lua git_toggle()<CR>", { noremap = true, silent = true })
 
 
-        local T_ter   = require('toggleterm.terminal').Terminal
-        local ter_opt = T_ter:new({
+        local T_ter    = require('toggleterm.terminal').Terminal
+        local ter_opt  = T_ter:new({
             -- dir = "git_dir",
             -- autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened
 
@@ -61,25 +61,46 @@ return {
 
         })
 
+        local T_ter1   = require('toggleterm.terminal').Terminal
+        local ter_opt1 = T_ter1:new({
+            -- dir = "git_dir",
+            -- autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+
+            -- cmd = "echo %",
+            -- direction = 'horizontal',
+            -- function to run on opening the terminal
+            on_open = function(term)
+                -- vim.cmd("startinsert!")
+                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+                vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+                vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-h>", [[<Cmd>wincmd h<CR>]],
+                    { noremap = true, silent = true })
+                vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-j>", [[<Cmd>wincmd j<CR>]],
+                    { noremap = true, silent = true })
+                vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-k>", [[<Cmd>wincmd k<CR>]],
+                    { noremap = true, silent = true })
+                vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-l>", [[<Cmd>wincmd h<CR>]],
+                    { noremap = true, silent = true })
+                -- vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-w>", [[<C-\><C-n><C-w>]], { noremap = true, silent = true })
+            end,
+            -- function to run on closing the terminal
+            on_close = function(term)
+                -- vim.cmd("startinsert!")
+            end,
+
+        })
         function ter_toggle()
             ter_opt:toggle()
+        end
+
+        function ter_toggle1()
+            ter_opt1:toggle()
         end
 
         vim.api.nvim_set_keymap("n", "<c-t>", "<Cmd>lua ter_toggle()<CR>", { noremap = true, silent = true })
         vim.api.nvim_set_keymap("t", "<c-t>", "<Cmd>lua ter_toggle()<CR>", { noremap = true, silent = true })
 
-        -- function _G.set_terminal_keymaps()
-        --     local opts = { buffer = 0 }
-        --     vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-        --     vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-        --     vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-        --     vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-        --     vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-        --     vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-        --     vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-        -- end
-        --
-        -- -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-        -- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+        vim.api.nvim_set_keymap("n", "<c-f>", "<Cmd>lua ter_toggle1()<CR>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("t", "<c-f>", "<Cmd>lua ter_toggle1()<CR>", { noremap = true, silent = true })
     end,
 }
