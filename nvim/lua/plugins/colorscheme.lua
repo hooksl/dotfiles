bb089bd03cd7b8d442cd120146ed57e0e114bb6a
hooks <liujiang047@gmail.com>
2024-07-27 14:10:43 +0800

    更换透明背景

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,39 @@
 return {
-    -- { 'joshdick/onedark.vim' },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "macchiato", -- latte, frappe, macchiato, mocha
-                background = {         -- :h background
-                    light = "latte",
-                    dark = "mocha",
-                },
-                transparent_background = true, -- disables setting the background color.
-                show_end_of_buffer = true,      -- shows the '~' characters after the end of buffers
-                term_colors = true,             -- sets terminal colors (e.g. `g:terminal_color_0`)
-                dim_inactive = {
-                    enabled = false,            -- dims the background color of inactive window
-                    shade = "dark",
-                    percentage = 0.15,          -- percentage of the shade to apply to the inactive window
-                },
-            })
-            -- vim.cmd.colorscheme "catppuccin"
-        end
-
-    },
+--     -- { 'joshdick/onedark.vim' },
+--     {
+--         "catppuccin/nvim",
+--         name = "catppuccin",
+--         priority = 1000,
+--         config = function()
+--             require("catppuccin").setup({
+--                 flavour = "macchiato", -- latte, frappe, macchiato, mocha
+--                 background = {         -- :h background
+--                     light = "latte",
+--                     dark = "mocha",
+--                 },
+--                 transparent_background = true, -- disables setting the background color.
+--                 show_end_of_buffer = true,      -- shows the '~' characters after the end of buffers
+--                 term_colors = true,             -- sets terminal colors (e.g. `g:terminal_color_0`)
+--                 dim_inactive = {
+--                     enabled = false,            -- dims the background color of inactive window
+--                     shade = "dark",
+--                     percentage = 0.15,          -- percentage of the shade to apply to the inactive window
+--                 },
+--             })
+--             -- vim.cmd.colorscheme "catppuccin"
+--         end
+--
+--     },
     {
         'folke/tokyonight.nvim',
         config = function()
             require("tokyonight").setup({
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
-                style = "storm",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                style = "night",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
                 light_style = "day",     -- The theme is used when the background is set to light
-                transparent = false,     -- Enable this to disable setting the background color
-                terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
+                transparent = true,     -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
                 styles = {
                     -- Style to be applied to different syntax groups
                     -- Value is any valid attr-list value for `:help nvim_set_hl`
